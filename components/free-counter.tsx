ed14f92333e1cfc76b09d03bb10d43dc295d873d
unknown <damianalcalayy@gmail.com>
2023-09-26 16:58:58 +0100

    fixed progress variants

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,54 +1,51 @@
-import { useEffect, useState } from "react";
-import { Card, CardContent } from "./ui/card";
-import { MAX_FREE_COUNTS } from "@/constants";
-import { Progress } from "./ui/progress";
-import { Button } from "./ui/button";
 import { Zap } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { MAX_FREE_COUNTS } from "@/constants";
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import { useProModal } from "@/hooks/use-pro-modal";
 
-interface FreeCounterProps {
-    apiLimitCount: number;
-    isPro: boolean;
-}
-
 export const FreeCounter = ({
-    apiLimitCount = 0,
-    isPro = false
-}:FreeCounterProps) => {
+  isPro = false,
+  apiLimitCount = 0,
+}: {
+  isPro: boolean,
+  apiLimitCount: number
+}) => {
+  const [mounted, setMounted] = useState(false);
+  const proModal = useProModal();
 
-    const proModal = useProModal()
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    const [isMounted, setIsMounted] = useState(false)
+  if (!mounted) {
+    return null;
+  }
+  
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
+  if (isPro) {
+    return null;
+  }
 
-    if (!isMounted) {
-        return null
-    }
-
-    if (isPro) {
-        return null
-    }
-
-
-    return (
-        <div className="px-3">
-            <Card className="bg-white/10 border-0">
-                <CardContent className="py-6">
-                    <div className="text-center text-sm text-white mb-4 space-y-2">
-                        <p>
-                            {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
-                        </p>
-                        <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100}/>
-                    </div>
-                    <Button onClick={proModal.onOpen} variant="premium" className="w-full">
-                        Upgrade
-                        <Zap className="w-4 h-4 ml-2 fill-white"/>
-                    </Button>
-                </CardContent>
-            </Card>
-        </div>
-    )
+  return (
+    <div className="px-3">
+      <Card className="bg-white/10 border-0">
+        <CardContent className="py-6">
+          <div className="text-center text-sm text-white mb-4 space-y-2">
+            <p>
+              {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+            </p>
+            <Progress className="h-3" value={(apiLimitCount / MAX_FREE_COUNTS) * 100} />
+          </div>
+          <Button onClick={proModal.onOpen} variant="premium" className="w-full">
+            Upgrade
+            <Zap className="w-4 h-4 ml-2 fill-white" />
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
