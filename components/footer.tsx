736030ba8139f35162dcb7b5b1e5ac6ebf3985ab
unknown <damianalcalayy@gmail.com>
2023-09-29 17:04:34 +0100

    fixed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link"
 const Footer = () => {
     return (
             <div className="mx-auto pb-16">
-                <p className="text-center text-lg text-black">
+                <p className="text-center text-lg text-white">
                     &copy; 2023 Harmony AI, Inc. All rights reserved.
                 </p>
                <Link href="https://damianalcalay.vercel.app/" target="_blank">
-                    <p className="text-center text-black mt-2 text-lg">
+                    <p className="text-center text-white mt-2 text-lg">
                        Powered by <span className="text-blue-700 font-bold hover:text-blue-700/80 transition">Damian Alcalay</span>
                     </p>
                </Link>
