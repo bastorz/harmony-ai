b0df0301272242ca05570d60e9dee2157be7cd62
unknown <damianalcalayy@gmail.com>
2023-09-30 23:34:05 +0100

    code revision on sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,58 +1,13 @@
 "use client"
-
 import Link from "next/link"
 import Image from "next/image"
 import { Montserrat } from "next/font/google"
 import { cn } from "@/lib/utils"
-import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react"
 import {usePathname} from "next/navigation"
 import { FreeCounter } from "./free-counter"
+import { routes } from "@/constants"
 
 const montserrat = Montserrat({weight: "600", subsets: ["latin"]})
-
-const routes = [
-    {
-        label: "Dashboard",
-        icon: LayoutDashboard,
-        href: "/dashboard",
-        color: "text-sky-500"
-    },
-    {
-        label: "Conversation",
-        icon: MessageSquare,
-        href: "/conversation",
-        color: "text-violet-700"
-    },
-    {
-        label: "Music Generation",
-        icon: Music,
-        href: "/music",
-        color: "text-emerald-500"
-    },
-    {
-        label: "Image Generation",
-        icon: ImageIcon,
-        href: "/image",
-        color: "text-pink-700"
-    },
-    {
-        label: "Video Generation",
-        icon: VideoIcon,
-        href: "/video",
-        color: "text-orange-700"
-    },
-    {
-        label: "Code Generation",
-        icon: Code,
-        href: "/code",
-        color: "text-green-700"
-    },
-    {
-        label: "Settings",
-        icon: Settings,
-        href: "/settings",
-    },
-]
 
 interface  SidebarProps {
     apiLimitCount: number
--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 
 export const MAX_FREE_COUNTS = 5;
 
@@ -64,5 +64,49 @@ export const testimonials = [
         avatar: "P",
         title: "Copywriter",
         description: "Was stucked looking for new ideas for my posts, but thanks to this tool now I'm back on track again"
+    },
+]
+
+export const routes = [
+    {
+        label: "Dashboard",
+        icon: LayoutDashboard,
+        href: "/dashboard",
+        color: "text-sky-500"
+    },
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        href: "/conversation",
+        color: "text-violet-700"
+    },
+    {
+        label: "Music Generation",
+        icon: Music,
+        href: "/music",
+        color: "text-emerald-500"
+    },
+    {
+        label: "Image Generation",
+        icon: ImageIcon,
+        href: "/image",
+        color: "text-pink-700"
+    },
+    {
+        label: "Video Generation",
+        icon: VideoIcon,
+        href: "/video",
+        color: "text-orange-700"
+    },
+    {
+        label: "Code Generation",
+        icon: Code,
+        href: "/code",
+        color: "text-green-700"
+    },
+    {
+        label: "Settings",
+        icon: Settings,
+        href: "/settings",
     },
 ]
